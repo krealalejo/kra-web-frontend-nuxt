ce12e35c550127a3f152fe89b3225675452bfca3
Kevin Real Alejo <krealalejo@gmail.com>
2026-04-19 16:42:13 +0200

    feat(20): install markdown packages and enable typography plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from 'tailwindcss'
+import typographyPlugin from '@tailwindcss/typography'
 
 export default {
   darkMode: 'class',
   content: [],
+  plugins: [typographyPlugin],
 } satisfies Config
