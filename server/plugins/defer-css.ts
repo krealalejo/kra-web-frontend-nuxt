29a857205a1de1d03b48dc0b7b3ce39060405b72
Kevin Real Alejo <krealalejo@gmail.com>
2026-05-02 03:20:00 +0200

    perf: defer Nuxt CSS async via Nitro render:html hook

---
+++ server/plugins/defer-css.ts
@@ -0,0 +1,11 @@
+export default defineNitroPlugin((nitroApp) => {
+  nitroApp.hooks.hook('render:html', (html) => {
+    html.head = html.head.map((chunk) =>
+      chunk.replace(
+        /<link rel="stylesheet" (href="\/_nuxt\/[^"]*\.css"[^>]*)>/g,
+        (_, attrs) =>
+          `<link rel="preload" as="style" ${attrs} onload="this.onload=null;this.rel='stylesheet'"><noscript><link rel="stylesheet" ${attrs}></noscript>`
+      )
+    )
+  })
+})
